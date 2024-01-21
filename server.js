d4bf99c98bc7976865575e0d92c9cd8a33b18007
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-21 13:42:08 -0800

    cleaning up and comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//imports and variables
 const express = require('express');
 
 const fs = require('fs');
@@ -6,18 +7,21 @@ const path = require('path');
 
 const port = process.env.PORT || 3001;
 
+// initialize express
 const app = express();
 
+// server static files
 app.use(express.static('public'));
 
+// start the server on specified port
 app.listen(port, () => {
     console.log(`Server listening on port ${port}! Visit http://localhost:${port} in your browser!`);
 });
 
+// define routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-
 
 app.post('/api/notes', (req, res) => {
     res.send('POST request to the notes page');
@@ -29,6 +33,7 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// default route
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
